801f6e5d9eb9d3ae84fc129414427c96ac725212
Lisa Mao <liisamao@gmail.com>
2018-07-12 00:53:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
-<h1 id="victory"> Lisa is the champion </h1>
+ var h1= document.createElement('h1')
+hi.textContent = "hi!"
+h1.setATtribute("id","victory")
